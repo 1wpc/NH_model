a2cc018c5aff6e20a900bdfe3bbf83922b4f3f4d
wpc <1992782991@qq.com>
2024-10-09 19:58:45 +0800

    read .mat

--- dp_dataset.py
+++ dp_dataset.py
@@ -1,9 +1,26 @@
 from torch.utils.data import Dataset, DataLoader
+import scipy.io as sio
+import os
 
 class ModmaDataset(Dataset):
-    def __init__(self, path, flag='train'):
+    def __init__(self, path='./modma', flag='train'):
         self.path = path
         self.flag = flag
+        mat_datas = []
+        labels = []
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                if file.endswith('.mat'):
+                    mat_datas.append(sio.loadmat(os.path.join(root, file)[file]))
+                    if file.startswith('0201'):
+                        labels.append(1)
+                    else:
+                        labels.append(0)
+
+
+
+        self.data = []
+
 
 
     def __len__(self):
